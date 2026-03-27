28e1f00188536996f17ef53f1164de2901e16ce4
Bruno Schaatsbergen <git@bschaatsbergen.com>
2026-03-27 14:10:47 +0100

    internal: implement Terraform package

--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 
 require (
 	cel.dev/expr v0.24.0 // indirect
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/ProtonMail/go-crypto v1.3.0 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
@@ -23,12 +25,17 @@ require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudflare/circl v1.6.1 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.12.2 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.8.0 // indirect
+	github.com/go-git/go-git/v5 v5.17.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
@@ -37,6 +44,7 @@ require (
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/cel-go v0.26.0 // indirect
 	github.com/google/gnostic-models v0.7.0 // indirect
@@ -50,22 +58,28 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
 	github.com/hashicorp/terraform-json v0.27.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pjbgf/sha1cd v0.3.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.22.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/zclconf/go-cty v1.17.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
@@ -98,6 +112,7 @@ require (
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.34.0 // indirect
 	k8s.io/apiextensions-apiserver v0.34.0 // indirect
--- internal/controller/workspace_controller.go
+++ internal/controller/workspace_controller.go
@@ -18,13 +18,23 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/hashicorp/terraform-exec/tfexec"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	magosprojectiov1alpha1 "github.com/magosproject/magos/api/v1alpha1"
+	"github.com/magosproject/magos/internal/terraform"
 )
 
 // WorkspaceReconciler reconciles a Workspace object
@@ -36,20 +46,132 @@ type WorkspaceReconciler struct {
 // +kubebuilder:rbac:groups=magosproject.io,resources=workspaces,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=magosproject.io,resources=workspaces/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=magosproject.io,resources=workspaces/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Workspace object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.1/pkg/reconcile
 func (r *WorkspaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the Workspace instance
+	var workspace magosprojectiov1alpha1.Workspace
+	if err := r.Get(ctx, req.NamespacedName, &workspace); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	log.Info("Reconciling Workspace", "name", workspace.Name, "namespace", workspace.Namespace)
+
+	// Create a temporary directory for the git clone
+	tmpDir, err := os.MkdirTemp("", fmt.Sprintf("magos-workspace-%s-*", workspace.Name))
+	if err != nil {
+		log.Error(err, "Failed to create temporary directory")
+		return ctrl.Result{}, err
+	}
+	defer os.RemoveAll(tmpDir) // Clean up the temp directory after reconciliation
+
+	log.Info("Cloning repository", "url", workspace.Spec.Source.RepoURL, "revision", workspace.Spec.Source.TargetRevision, "dir", tmpDir)
+
+	cloneOpts := &git.CloneOptions{
+		URL:   workspace.Spec.Source.RepoURL,
+		Depth: 1, // Shallow clone for performance and reduced disk/memory usage
+	}
+
+	// Configure Git authentication if a SecretRef is provided
+	if workspace.Spec.Source.SecretRef != nil {
+		var secret corev1.Secret
+		secretKey := types.NamespacedName{
+			Name:      workspace.Spec.Source.SecretRef.Name,
+			Namespace: workspace.Namespace, // Assuming secret is in the same namespace
+		}
+
+		if err := r.Get(ctx, secretKey, &secret); err != nil {
+			log.Error(err, "Failed to fetch Git authentication secret", "secret", secretKey.Name)
+			return ctrl.Result{}, err
+		}
+
+		// Basic Auth (HTTPS)
+		username := string(secret.Data["username"])
+		password := string(secret.Data["password"])
+
+		if username != "" || password != "" {
+			cloneOpts.Auth = &http.BasicAuth{
+				Username: username,
+				Password: password,
+			}
+		}
+		// Note: SSH auth (e.g. using ssh.PublicKeys) can be added here as an alternative
+	}
+
+	// Clone the repository
+	repo, err := git.PlainClone(tmpDir, false, cloneOpts)
+	if err != nil {
+		log.Error(err, "Failed to clone repository")
+		return ctrl.Result{}, err
+	}
+
+	// Checkout the target revision
+	worktree, err := repo.Worktree()
+	if err != nil {
+		log.Error(err, "Failed to get worktree")
+		return ctrl.Result{}, err
+	}
+
+	// Try to resolve the revision to a hash
+	hash, err := repo.ResolveRevision(plumbing.Revision(workspace.Spec.Source.TargetRevision))
+	if err != nil {
+		log.Error(err, "Failed to resolve target revision")
+		return ctrl.Result{}, err
+	}
+
+	err = worktree.Checkout(&git.CheckoutOptions{
+		Hash: *hash,
+	})
+	if err != nil {
+		log.Error(err, "Failed to checkout revision")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("Successfully checked out repository", "hash", hash.String())
+
+	// Set up the Terraform working directory
+	workDir := tmpDir
+	if workspace.Spec.Source.Path != "" && workspace.Spec.Source.Path != "." {
+		workDir = filepath.Join(tmpDir, workspace.Spec.Source.Path)
+	}
+
+	log.Info("Initializing Terraform client", "version", workspace.Spec.Terraform.Version, "workDir", workDir)
+
+	// Create Terraform client and automatically install the specified binary version
+	tfClient, err := terraform.NewClientFromInstall(ctx, workDir, workspace.Spec.Terraform.Version, "")
+	if err != nil {
+		log.Error(err, "Failed to initialize Terraform client")
+		return ctrl.Result{}, err
+	}
+
+	// Run Terraform Init
+	log.Info("Running Terraform Init")
+	var initOpts []tfexec.InitOption
+	if err := tfClient.Init(ctx, initOpts...); err != nil {
+		log.Error(err, "Terraform Init failed")
+		return ctrl.Result{}, err
+	}
+
+	// Run Terraform Plan
+	log.Info("Running Terraform Plan")
+	var planOpts []tfexec.PlanOption
+	if workspace.Spec.Terraform.TfvarsPath != "" {
+		// tfvars path should be evaluated relative to the repository root
+		tfvarsFile := filepath.Join(tmpDir, workspace.Spec.Terraform.TfvarsPath)
+		planOpts = append(planOpts, tfexec.VarFile(tfvarsFile))
+	}
+
+	hasChanges, err := tfClient.Plan(ctx, "", planOpts...)
+	if err != nil {
+		log.Error(err, "Terraform Plan failed")
+		return ctrl.Result{}, err
+	}
+
+	log.Info("Terraform Plan completed", "hasChanges", hasChanges)
 
 	return ctrl.Result{}, nil
 }
